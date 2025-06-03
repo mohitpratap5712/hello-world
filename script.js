6c9a878df94735b07da7901c4c325ca2c208b944
Mohit Singh <mohitpratap5712@gmail.com>
2025-06-03 23:57:50 +0530

    js upgraded

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 alert("hello world ");
 console.log("hello mohit banna");
 
+let boxess= document.getElementsByClassName("container");
+console.log("boxes");
+container[2].style.backgroundcolor="black";
