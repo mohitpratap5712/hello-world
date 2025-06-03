6972da05ebff5b4d5b432a7a333a0d87e171838a
Mohit Singh <mohitpratap5712@gmail.com>
2025-06-04 00:03:31 +0530

    added title on website

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ console.log("hello mohit banna");
 let boxess= document.getElementsByClassName("container");
 console.log("boxes");
 container[2].style.backgroundcolor="black";
+document.title = "mohit banna site" ;
